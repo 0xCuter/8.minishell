5ccc85bb2f5f4e7fc87bdec7c925043f523a81fe
vvandenb <vascovandenbosch12@gmail.com>
2022-03-22 12:43:53 +0100

    added ctrl c ignore, added command location searching

--- srcs/main.c
+++ srcs/main.c
@@ -6,44 +6,83 @@
 /*   By: vvandenb <vvandenb@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/22 09:06:18 by vvandenb          #+#    #+#             */
-/*   Updated: 2022/03/22 10:56:26 by vvandenb         ###   ########.fr       */
+/*   Updated: 2022/03/22 12:42:53 by vvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	error(const char *error)
+//Prints the error and exits with `errno`
+static void	error(const char *error)
 {
 	perror(error);
-	return (errno);
+	exit (errno);
+}
+
+//Searches for the command passed and executes it in a child process if found
+//Returns the pid of the child, or else 0
+static int	execute(char *line, char **path_split)
+{
+	int			pid;
+	int			size;
+	char		*command;
+	char		found_command_location;
+	struct stat	test;
+
+	found_command_location = 0;
+	while (found_command_location == 0 && *path_split != NULL)
+	{
+		size = ft_strlen(*path_split) + ft_strlen(line) + 2;
+		command = malloc(size * sizeof(char));
+		ft_strlcpy(command, *path_split, size);
+		ft_strlcat(command, "/", size);
+		ft_strlcat(command, line, size);
+		if (stat(command, &test) == 0)
+		{
+			found_command_location = 1;
+			break ;
+		}
+		free(command);
+		++path_split;
+	}
+	if (found_command_location)
+	{
+		pid = fork();
+		if (pid == -1)
+		{
+			free(command);
+			error("FORK");
+		}
+		if (pid == 0)
+		{
+			char *test[] = {"ls", NULL};
+			if (execve(command, test, NULL))
+				error("EXECVE");
+		}
+		free(command);
+		return (pid);
+	}
+	return (0);
 }
 
 int	main()
 {
-	char 		*line;
-	struct stat	buf;
-	int			i;
-	int			e;
+	char	*path;
+	char	**path_split;
+	char	*line;
 
-	i = 0;
-	while (i < 3)
+	path = getenv("PATH");
+	if (path == NULL)
+		return (1);
+	path_split = ft_split(path, ':');
+	if (path_split == NULL)
+		error("MALLOC");
+	setup_signals();
+	while (1)
 	{
 		line = readline("Minishell$ ");
-		e = stat(line, &buf);
-		if (e)
-			perror("STAT");
-		else
-		{
-			int pid = fork();
-			if (pid == 0)
-			{
-				char *test[] = {"ls", NULL};
-				if (execve(line, test, NULL))
-					return (error("EXECVE"));
-			}
-			wait(NULL);
-		}
+		execute(line, path_split);
 		free(line);
-		++i;
+		wait(NULL);
 	}
 }
--- srcs/signals.c
+++ srcs/signals.c
@@ -1,32 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   minishell.h                                        :+:      :+:    :+:   */
+/*   signals.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vvandenb <vvandenb@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/22 09:08:58 by vvandenb          #+#    #+#             */
-/*   Updated: 2022/03/22 12:34:48 by vvandenb         ###   ########.fr       */
+/*   Created: 2022/03/22 12:07:05 by vvandenb          #+#    #+#             */
+/*   Updated: 2022/03/22 12:38:12 by vvandenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MINISHELL_H
-# define MINISHELL_H
+#include "minishell.h"
 
-# include <unistd.h>
-# include <stdio.h>
-# include <stdlib.h>
-# include <readline/readline.h>
-# include <sys/types.h>
-# include <dirent.h>
-# include <errno.h>
-# include <sys/stat.h>
-# include <sys/wait.h>
-# include <readline/history.h>
+void	do_nothing()
+{}
 
-# include "libft.h"
+int	setup_signals()
+{
+	struct sigaction	sig;
 
-//signals.c
-int	setup_signals(void);
-
-#endif
+	sig.sa_sigaction = do_nothing;
+	sig.sa_flags = 0;
+	sigaction(SIGINT, &sig, NULL);
+	return (0);
+}
